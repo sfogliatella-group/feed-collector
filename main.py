9317d8f7e556f6e4baf124ebce6d3be6be6f9a08
Fulvio <fulviodenza823@gmail.com>
2019-08-26 19:55:59 +0200

    Added basics for TUI and gitignore

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import twitter
+import sys
+sys.path.insert(0, "controller/")
+sys.path.insert(0, "view/")
 
-api = twitter.Api(consumer_key='LMjlku3YFTz6iZ8zPggN7A8At',
-                  consumer_secret='z1iDWc34iVMOQIbRMAo4a41NK5TDdvFwXVvuDbMW59nMI855dT',
-                  access_token_key='834036559263461376-7KpnKaq5Bk8c0DSu8fpQtmYy48ACp56',
-                  access_token_secret='YviRSCRxFog119yBdQDDyq10eXLQYXQDKgRto0nBF55Nt')
-print(api.VerifyCredentials())
+import feedGetter
+import form
+
+feedGetter.allNews()
--- view/changeTitleBar.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.title("feed-collector")
-root.iconbitmap('baseline-drag_indicator-24px.xbm')
-root.mainloop()
